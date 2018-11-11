05a0f5d4dc8a0b054cbc727b7c7a856d14eb6651
Ilia Choly <ilia.choly@gmail.com>
2018-11-11 18:08:54 -0500

    update the compiler to use the lexer and parser

--- compiler.go
+++ compiler.go
@@ -1,29 +1,60 @@
-package compiler
+package cc
 
 import (
 	"fmt"
-	"regexp"
-	"strconv"
+	"strings"
+
+	"github.com/icholy/cc/ast"
+	"github.com/icholy/cc/parser"
 )
 
-const outputfmt = `
-.globl  _main
-_main:
-	movl    $%d, %%eax
-	leave
-	ret
-`
-
-var re = regexp.MustCompile(`int\s+main\s*\(\s*\)\s*{\s*return\s+(?P<ret>[0-9]+)\s*;\s*}`)
-
 func Compile(src string) (string, error) {
-	match := re.FindStringSubmatch(src)
-	if len(match) != 2 {
-		return "", fmt.Errorf("cannot find return value")
-	}
-	n, err := strconv.Atoi(match[1])
+	prog, err := parser.Parse(src)
 	if err != nil {
-		return "", nil
+		return "", err
 	}
-	return fmt.Sprintf(outputfmt, n), nil
+	var asm strings.Builder
+	if err := compileProgram(prog, &asm); err != nil {
+		return "", err
+	}
+	return asm.String(), nil
+}
+
+func compileProgram(p *ast.Program, asm *strings.Builder) error {
+	switch stmt := p.Body.(type) {
+	case *ast.Function:
+		return compileFunction(stmt, asm)
+	default:
+		return fmt.Errorf("cannot compile: %s", p.Body)
+	}
+	return nil
+}
+
+func compilerExpr(expr ast.Expr, asm *strings.Builder) error {
+	switch expr := expr.(type) {
+	case *ast.IntLiteral:
+		fmt.Fprintf(asm, "movl $%d, %%eax\n", expr.Value)
+	default:
+		return fmt.Errorf("cannot compile: %s", expr)
+	}
+	return nil
+}
+
+func compileStmt(stmt ast.Stmt, asm *strings.Builder) error {
+	switch stmt := stmt.(type) {
+	case *ast.Return:
+		if err := compilerExpr(stmt.Value, asm); err != nil {
+			return err
+		}
+		fmt.Fprintf(asm, "ret\n")
+	default:
+		return fmt.Errorf("cannot compile: %s", stmt)
+	}
+	return nil
+}
+
+func compileFunction(f *ast.Function, asm *strings.Builder) error {
+	fmt.Fprintf(asm, ".globl _%s\n", f.Name)
+	fmt.Fprintf(asm, "_%s:\n", f.Name)
+	return compileStmt(f.Body, asm)
 }
--- compiler_test.go
+++ compiler_test.go
@@ -1,4 +1,4 @@
-package compiler
+package cc
 
 import (
 	"fmt"
