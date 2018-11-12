b289e336100bc9b67b641d867aef5fa9f6a92a05
Ilia Choly <ilia.choly@gmail.com>
2018-11-11 19:14:57 -0500

    refactor parser tests

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,63 +1,35 @@
 package parser
 
 import (
+	"fmt"
 	"io/ioutil"
+	"path/filepath"
 	"testing"
 
 	"gotest.tools/assert"
 )
 
-func TestStage1(t *testing.T) {
-	tests := []struct {
-		SrcPath string
-		Valid   bool
-	}{
-		// valid cases
-		{SrcPath: "../testdata/stage_1/valid/multi_digit.c", Valid: true},
-		{SrcPath: "../testdata/stage_1/valid/newlines.c", Valid: true},
-		{SrcPath: "../testdata/stage_1/valid/spaces.c", Valid: true},
-		{SrcPath: "../testdata/stage_1/valid/return_0.c", Valid: true},
-		{SrcPath: "../testdata/stage_1/valid/return_2.c", Valid: true},
-
-		// valid cases
-		{SrcPath: "../testdata/stage_1/invalid/missing_paren.c", Valid: false},
-		{SrcPath: "../testdata/stage_1/invalid/missing_retval.c", Valid: false},
-		{SrcPath: "../testdata/stage_1/invalid/no_brace.c", Valid: false},
-		{SrcPath: "../testdata/stage_1/invalid/no_space.c", Valid: false},
-		{SrcPath: "../testdata/stage_1/invalid/wrong_case.c", Valid: false},
-	}
-	for _, tt := range tests {
-		t.Run(tt.SrcPath, func(t *testing.T) {
-			src, err := ioutil.ReadFile(tt.SrcPath)
-			assert.NilError(t, err)
-			_, err = Parse(string(src))
-			if tt.Valid {
-				assert.NilError(t, err)
-			} else {
-				assert.Assert(t, err != nil)
-			}
-		})
-	}
+func TestValidParsing(t *testing.T) {
+	AssertParsingStage(t, 1)
+	AssertParsingStage(t, 2)
 }
-func TestStage2(t *testing.T) {
-	tests := []struct {
-		SrcPath string
-		Valid   bool
-	}{
-		// valid cases
-		{SrcPath: "../testdata/stage_2/valid/bitwise_zero.c", Valid: true},
-		{SrcPath: "../testdata/stage_2/valid/bitwise.c", Valid: true},
-		{SrcPath: "../testdata/stage_2/valid/neg.c", Valid: true},
-		{SrcPath: "../testdata/stage_2/valid/nested_ops.c", Valid: true},
-		{SrcPath: "../testdata/stage_2/valid/nested_ops_2.c", Valid: true},
-		{SrcPath: "../testdata/stage_2/valid/not_five.c", Valid: true},
-		{SrcPath: "../testdata/stage_2/valid/not_zero.c", Valid: true},
 
-		// invalid cases
-		{SrcPath: "../testdata/stage_2/invalid/missing_const.c", Valid: false},
-		{SrcPath: "../testdata/stage_2/invalid/missing_semicolon.c", Valid: false},
-		{SrcPath: "../testdata/stage_2/invalid/nested_missing_const.c", Valid: false},
-		{SrcPath: "../testdata/stage_2/invalid/wrong_order.c", Valid: false},
+type validityTest struct {
+	SrcPath string
+	Valid   bool
+}
+
+func AssertParsingStage(t *testing.T, stage int) {
+	var tests []validityTest
+	valid, err := filepath.Glob(fmt.Sprintf("../testdata/stage_%d/valid/*.c", stage))
+	assert.NilError(t, err)
+	for _, path := range valid {
+		tests = append(tests, validityTest{path, true})
+	}
+	invalid, err := filepath.Glob(fmt.Sprintf("../testdata/stage_%d/invalid/*.c", stage))
+	assert.NilError(t, err)
+	for _, path := range invalid {
+		tests = append(tests, validityTest{path, false})
 	}
 	for _, tt := range tests {
 		t.Run(tt.SrcPath, func(t *testing.T) {
